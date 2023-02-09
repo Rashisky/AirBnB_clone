fab7c71a725f915156342caa1d53676d33f6548f
Rashisky <rashisky007@gmail.com>
2023-02-09 18:56:49 +0100

    BaseModel Update

--- models/base_model.py
+++ models/base_model.py
@@ -4,24 +4,36 @@
 import uuid
 import datetime
 
+
 class BaseModel:
     """The base class that contains all methods and attributes"""
-    
-    def __init__(self):
-        """Instantiate the id, when id is created and updated"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        
+
+    def __init__(self, *args, **kwargs):
+        """Instantiate the id, when id is created and updated
+
+            Args: attributes of an instance
+            Kwargs: dictionary key/value attributes of an instance
+        """
+        if (kwargs):
+            self.id = kwargs['id']
+            self.created_at = datetime.datetime.strptime(
+                kwargs['created_at'], "%Y-%d-%mT%H:%M:%S.%f")
+            self.updated_at = datetime.datetime.strptime(
+                kwargs['updated_at'], "%Y-%d-%mT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>"""
         result = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
         return result
-    
+
     def save(self):
         """updates the public instance attribute and save"""
         self.updated_at = datetime.datetime.now()
-    
+
     def to_dict(self):
         """returns a dictionary containing all keys/value of __dict__
         of the instance"""
